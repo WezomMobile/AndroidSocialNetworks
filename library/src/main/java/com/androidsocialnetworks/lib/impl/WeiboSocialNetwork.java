9c6399a9da93438a4cd0d03f853cf3e59a8d9958
Vitaliy Oskalenko <oskalenkoVit@ukr.net>
2015-10-18 17:04:54 +0100

    Added WeiboSocialNetwork

--- library/src/main/java/com/androidsocialnetworks/lib/impl/WeiboSocialNetwork.java
+++ library/src/main/java/com/androidsocialnetworks/lib/impl/WeiboSocialNetwork.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.androidsocialnetworks.lib.AccessToken;
 import com.androidsocialnetworks.lib.OAuthSocialNetwork;
 import com.androidsocialnetworks.lib.AccessTokenKeeper;
+import com.androidsocialnetworks.lib.SocialPerson;
 import com.androidsocialnetworks.lib.listener.OnLoginCompleteListener;
+import com.androidsocialnetworks.lib.listener.OnPostingCompleteListener;
+import com.androidsocialnetworks.lib.listener.OnRequestSocialPersonCompleteListener;
 import com.sina.weibo.sdk.auth.AuthInfo;
 import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.WeiboAuthListener;
@@ -17,6 +21,10 @@ import com.sina.weibo.sdk.auth.sso.SsoHandler;
 import com.sina.weibo.sdk.exception.WeiboException;
 import com.sina.weibo.sdk.net.RequestListener;
 import com.sina.weibo.sdk.openapi.LogoutAPI;
+import com.sina.weibo.sdk.openapi.StatusesAPI;
+import com.sina.weibo.sdk.openapi.UsersAPI;
+import com.sina.weibo.sdk.openapi.models.ErrorInfo;
+import com.sina.weibo.sdk.openapi.models.User;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,40 +37,53 @@ import org.json.JSONObject;
  */
 public class WeiboSocialNetwork extends OAuthSocialNetwork {
 
+    private static final String TAG = WeiboSocialNetwork.class.getSimpleName();
+
     public static final int ID = 5;
-    private static final String REDIRECT_URL = " http://www.sina.com";
+    private static final String REDIRECT_URL = "https://api.weibo.com/oauth2/default.html";
+    public static final String SCOPE = "email,direct_messages_read,direct_messages_write,"
+            + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
+            + "follow_app_official_microblog,"
+            + "invitation_write ";
 
-    private final String fConsumerKey;
-    private final Activity fContext;
-
+    private Activity mContext;
+    private String mConsumerKey;
     private AuthInfo mAuthInfo;
     private SsoHandler mSsoHandler;
     private Oauth2AccessToken mAccessToken;
     private LogOutRequestListener mLogoutRequestListener = new LogOutRequestListener();
+    private UsersAPI mUsersAPI;
+    private StatusesAPI mStatusesAPI;
 
-    public static final String SCOPE = " email,direct_messages_read,direct_messages_write, "
-            + " friendships_groups_read,friendships_groups_write,statuses_to_me_read, "
-            + " follow_app_official_microblog, "
-            + " invitation_write ";
 
     protected WeiboSocialNetwork(Fragment fragment, String consumerKey) {
         super(fragment);
 
-        fContext = fragment.getActivity();
-        fConsumerKey = consumerKey;
+        mContext = fragment.getActivity();
+        mConsumerKey = consumerKey;
 
-        if (TextUtils.isEmpty(fConsumerKey)) {
+        if (TextUtils.isEmpty(mConsumerKey)) {
             throw new IllegalArgumentException("consumerKey is invalid");
         }
 
-        initWeiboClient();
+        initWeibo();
     }
 
-    private void initWeiboClient() {
-        mAuthInfo = new AuthInfo(fContext, fConsumerKey, REDIRECT_URL, SCOPE);
-        mSsoHandler = new SsoHandler(fContext, mAuthInfo);
+    private void initWeibo() {
+        mAuthInfo = new AuthInfo(mContext, mConsumerKey, REDIRECT_URL, SCOPE);
+        mSsoHandler = new SsoHandler(mContext, mAuthInfo);
 
-        mAccessToken = AccessTokenKeeper.readAccessToken(fContext);
+        mAccessToken = AccessTokenKeeper.readAccessToken(mContext);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return mAccessToken != null && mAccessToken.isSessionValid();
+    }
+
+    @Override
+    public int getID() {
+        return ID;
     }
 
     @Override
@@ -70,6 +91,39 @@ public class WeiboSocialNetwork extends OAuthSocialNetwork {
         super.requestLogin(onLoginCompleteListener);
 
         mSsoHandler.authorize(new AuthListener());
+    }
+
+    @Override
+    public void logout() {
+        if (mAccessToken != null && mAccessToken.isSessionValid()) {
+            new LogoutAPI(mContext, mConsumerKey, mAccessToken).logout(mLogoutRequestListener);
+        }
+    }
+
+    @Override
+    public void requestCurrentPerson(OnRequestSocialPersonCompleteListener onRequestSocialPersonCompleteListener) {
+        super.requestCurrentPerson(onRequestSocialPersonCompleteListener);
+
+        if (mAccessToken == null) {
+            if (mLocalListeners.get(REQUEST_GET_CURRENT_PERSON) != null) {
+                mLocalListeners.get(REQUEST_GET_CURRENT_PERSON).onError(getID(),
+                        REQUEST_GET_CURRENT_PERSON, "please login first", null);
+            }
+            return;
+        }
+
+        long uid = Long.parseLong(mAccessToken.getUid());
+        mUsersAPI.show(uid, mPersonInfoListener);
+
+        mUsersAPI = new UsersAPI(mContext, mConsumerKey, mAccessToken);
+    }
+
+    @Override
+    public void requestPostMessage(String message, OnPostingCompleteListener onPostingCompleteListener) {
+        super.requestPostMessage(message, onPostingCompleteListener);
+
+        mStatusesAPI = new StatusesAPI(mContext, mConsumerKey, mAccessToken);
+        mStatusesAPI.update(message, null, null, mPostMessageListener);
     }
 
     @Override
@@ -82,27 +136,8 @@ public class WeiboSocialNetwork extends OAuthSocialNetwork {
     }
 
     @Override
-    public boolean isConnected() {
-        return false;
-    }
-
-    @Override
-    public void logout() {
-        if (mAccessToken != null && mAccessToken.isSessionValid()) {
-            new LogoutAPI(fContext, fConsumerKey,
-                    AccessTokenKeeper.readAccessToken(fContext)).logout(mLogoutRequestListener);
-        } else {
-        }
-    }
-
-    @Override
-    public int getID() {
-        return 0;
-    }
-
-    @Override
     public AccessToken getAccessToken() {
-        return null;
+        return new AccessToken(AccessTokenKeeper.readAccessToken(mContext).getToken(), null);
     }
 
     private class AuthListener implements WeiboAuthListener {
@@ -112,9 +147,11 @@ public class WeiboSocialNetwork extends OAuthSocialNetwork {
             mAccessToken = Oauth2AccessToken.parseAccessToken(values);
             if (mAccessToken.isSessionValid()) {
                 // Save token to SharedPreferences
-                AccessTokenKeeper.writeAccessToken(fContext, mAccessToken);
-            } else {
-                String code = values.getString("code");
+                AccessTokenKeeper.writeAccessToken(mContext, mAccessToken);
+                if (mLocalListeners.get(REQUEST_LOGIN) != null) {
+                    ((OnLoginCompleteListener) mLocalListeners.get(REQUEST_LOGIN)).onLoginSuccess(getID());
+                    mLocalListeners.remove(REQUEST_LOGIN);
+                }
             }
         }
 
@@ -124,6 +161,7 @@ public class WeiboSocialNetwork extends OAuthSocialNetwork {
 
         @Override
         public void onWeiboException(WeiboException e) {
+            handleError(e, REQUEST_LOGIN);
         }
     }
 
@@ -132,20 +170,72 @@ public class WeiboSocialNetwork extends OAuthSocialNetwork {
         public void onComplete(String response) {
             if (!TextUtils.isEmpty(response)) {
                 try {
-                    JSONObject obj = new JSONObject(response);
-                    String value = obj.getString("result");
-                    if ("true".equalsIgnoreCase(value)) {
-                        AccessTokenKeeper.clear(fContext);
+                    JSONObject responseJson = new JSONObject(response);
+                    String result = responseJson.getString("result");
+                    if (result.equalsIgnoreCase("true")) {
+                        AccessTokenKeeper.clear(mContext);
                         mAccessToken = null;
                     }
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    Log.e(TAG, e.getMessage());
                 }
             }
         }
 
         @Override
         public void onWeiboException(WeiboException e) {
+            handleError(e, null);
+        }
+    }
+
+    private RequestListener mPersonInfoListener = new RequestListener() {
+        @Override
+        public void onComplete(String response) {
+            if (!TextUtils.isEmpty(response)) {
+                User user = User.parse(response);
+                if (user != null) {
+                    SocialPerson socialPerson = new SocialPerson();
+                    socialPerson.id = user.id;
+                    socialPerson.name = user.name;
+                    socialPerson.nickname = user.screen_name;
+                    socialPerson.profileURL = user.profile_url;
+                    socialPerson.avatarURL = user.avatar_large;
+
+                    ((OnRequestSocialPersonCompleteListener) mLocalListeners.get(REQUEST_GET_CURRENT_PERSON))
+                            .onRequestSocialPersonSuccess(getID(), socialPerson);
+                    mLocalListeners.remove(REQUEST_GET_CURRENT_PERSON);
+                }
+            }
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            handleError(e, REQUEST_GET_CURRENT_PERSON);
+        }
+    };
+
+    private RequestListener mPostMessageListener = new RequestListener() {
+        @Override
+        public void onComplete(String response) {
+            ((OnPostingCompleteListener) mLocalListeners.get(REQUEST_POST_MESSAGE)).onPostSuccessfully(getID());
+            mLocalListeners.remove(REQUEST_POST_MESSAGE);
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            handleError(e, REQUEST_POST_MESSAGE);
+        }
+    };
+
+    private void handleError(WeiboException e, String requestId) {
+        Log.e(TAG, e.getMessage());
+        if (!TextUtils.isEmpty(requestId)) {
+            ErrorInfo errorInfo = ErrorInfo.parse(e.getMessage());
+            if (mLocalListeners.get(requestId) != null) {
+                mLocalListeners.get(requestId).onError(
+                        getID(), requestId, errorInfo.toString(), null);
+                mLocalListeners.remove(REQUEST_LOGIN);
+            }
         }
     }
 }
